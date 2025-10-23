9fe3df1f0a31174a2823f4c223261ed59ff4cfcd
haznooo <rxzr99x@gmail.com>
2025-10-23 16:09:31 +0300

    added application types in buisness and data access layers

---
+++ BusinessLayer/clsApplicationTypes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class clsApplicationTypes
+    {
+
+        static public DataTable GetAllApplicationTypes()
+        {
+
+            return clsApplicationTypes.GetAllApplicationTypes();
+
+        }
+    }
+}
---
+++ Data Access Layer/clsApplicationTypesDataAccess.cs
@@ -0,0 +1,53 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer
+{
+    public class clsApplicationTypesDataAccess
+    {
+        static public DataTable GetApplicationTypes()
+        {
+
+            DataTable dt = new DataTable();
+
+            string Query = "Select * from ApplicationTypes";
+
+            SqlConnection Connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            SqlCommand command = new SqlCommand(Query, Connection);
+
+            try
+            {
+                Connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+
+                    dt.Load(reader);
+
+                }
+
+
+            }
+            catch (Exception ex) { }
+
+            finally
+            {
+
+                Connection.Close();
+
+            }
+
+            return dt;
+
+        }
+    }
+}
