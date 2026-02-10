391159f27e61a8f0f8ec428c53673693653ca0d9
haznooo <rxzr99x@gmail.com>
2026-02-10 14:54:34 +0300

    drivers data access and business added

---
+++ BusinessLayer/clsDriver.cs
@@ -0,0 +1,109 @@
+﻿using Data_Access_Layer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class clsDriver
+    {
+        enum enMode {add,update }
+        enMode _Mode = enMode.update;
+        public int DriverID;
+        public int CreatedByUserID;
+        public DateTime CreatedDate;
+        public int PersonID;
+        clsPerson PersonInfo;
+
+        public clsDriver() {
+        
+            DriverID = -1;
+            CreatedByUserID = -1;
+            CreatedDate = DateTime.Now;
+            PersonInfo = new clsPerson();
+            PersonID = -1;
+            _Mode = enMode.add;
+        
+        }
+        public clsDriver(int DriverID,int CreatedByUserID,DateTime CreaetDate,int PersonID) 
+        {
+
+            this.DriverID = DriverID;
+            this.CreatedByUserID = CreatedByUserID;
+            this.CreatedDate = CreaetDate;
+            this.PersonID = PersonID;
+            this.PersonInfo = clsPerson.GetPersonInfoByID(PersonID);
+
+            _Mode = enMode.update;
+        }
+
+
+        static public clsDriver GetDriverInfoByID(int DriverID)
+            {
+            int CreatedByUserID = -1, PersonID =-1;
+            DateTime CreateDate = DateTime.Now;
+
+
+            if (clsDrivierDataAccess.GetDriverInfoByDrivierID(DriverID, ref PersonID, ref CreatedByUserID, ref CreateDate)) 
+            {
+            return new clsDriver(DriverID,CreatedByUserID,CreateDate,PersonID);
+            }
+            return null;
+        
+        }
+
+        static public clsDriver GetDriverInfoByPersonID(int PersonID )
+        {
+            int CreatedByUserID = -1, DriverID = -1;
+            DateTime CreateDate = DateTime.Now;
+
+
+            if (clsDrivierDataAccess.GetDriverInfoByPersonID(ref DriverID,PersonID, ref CreatedByUserID, ref CreateDate))
+            {
+                return new clsDriver(DriverID, CreatedByUserID, CreateDate, PersonID);
+            }
+            return null;
+
+        }
+
+        static public DataTable GetAllDrivers()   
+        {
+        return clsDrivierDataAccess.GetAllDriversInf();
+        }
+
+        public bool Save() 
+        {
+
+            switch (_Mode) 
+            {
+
+                case enMode.add:
+                    {
+                        if (_AddNewDriver() > 0) return true;
+                        else return false;
+                    
+                    }
+                case enMode.update:
+                    return _UpdateDriver();
+
+                default:return false;
+            }
+
+        }
+
+        private int _AddNewDriver()
+        {
+            return clsDrivierDataAccess.addNewDriver(ref this.PersonID, ref this.CreatedByUserID, ref this.CreatedDate);
+        }
+        private bool _UpdateDriver()
+        {
+           return clsDrivierDataAccess.UpdateDriver(this.DriverID,this.PersonID, this.CreatedByUserID, this.CreatedDate);
+        
+        }
+
+
+    }
+}
---
+++ Data Access Layer/clsDrivierDataAccess.cs
@@ -0,0 +1,197 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer
+{
+    public class clsDrivierDataAccess
+    {
+
+        public static bool GetDriverInfoByDrivierID(int DriverID, ref int personID, ref int CreatedByUserID,
+            ref DateTime CreateDate)
+        {
+            string query = "select * from Drivers where DriverID = @driverID";
+
+            SqlConnection Connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            SqlCommand Coomand = new SqlCommand(query, Connection);
+            Coomand.Parameters.AddWithValue("@driverID", DriverID);
+            bool isFound = false;
+
+            try
+            {
+
+                Connection.Open();
+                SqlDataReader reader = Coomand.ExecuteReader();
+
+                while (reader.HasRows)
+                {
+
+                    personID = Convert.ToInt32(reader["PersonID"]);
+                    CreatedByUserID = Convert.ToInt32(reader["CreatedByUserID"]);
+                    CreateDate = Convert.ToDateTime(reader["CreateDate"]);
+                    isFound = true;
+                }
+                reader.Close();
+
+            }
+            catch (Exception ex)
+            {
+            }
+            finally {
+
+                Connection.Close();
+            }
+            return isFound;
+
+
+
+        }
+
+        public static DataTable GetAllDriversInf() 
+      {
+        
+            DataTable dt = new DataTable();
+            string query = "select * from Drivers_View";
+
+            SqlConnection Connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            SqlCommand Coomand = new SqlCommand(query,Connection);
+
+            try
+            {
+
+                Connection.Open();
+                SqlDataReader reader = Coomand.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+
+                    dt.Load(reader);
+                }
+                reader.Close() ;
+
+
+            }
+            catch (Exception ex) { }
+            finally { Connection.Close(); }
+
+
+            return dt;
+
+        }
+
+        public static int addNewDriver(ref int personID, ref int CreatedByUserID,
+            ref DateTime CreateDate)     
+        {
+            string query = "insert into Drivers (personID,CreatedByUserID,CreateDate) values (@personID,@CreatedByUserID,@CreateDate);SELECT SCOPE_IDENTITY();";
+            SqlConnection Connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            SqlCommand Coomand = new SqlCommand( query,Connection);
+
+            Coomand.Parameters.AddWithValue("@personID", personID);
+            Coomand.Parameters.AddWithValue("@CreatedByUserID", CreatedByUserID);
+            Coomand.Parameters.AddWithValue("@CreateDate", CreateDate);
+
+            int NewID = -1;
+
+            try 
+            {
+                Connection.Open();
+                object result = Coomand.ExecuteScalar();
+
+                if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                {
+                    NewID = insertedID;
+                }
+
+
+            }
+            catch (Exception ex) { }
+            finally { Connection.Close(); }
+            return NewID;
+
+
+        }
+
+        public static bool UpdateDriver(int driverID, int personID, int CreatedByUserID,
+            DateTime CreateDate)
+        {
+
+            string query = @"update Drivers 
+                             set PersonID = @PersonID,
+                             CreatedByUserID = @CreatedByUserID,
+                             CreateDate = @CreateDate 
+                             where DriverID = @DriverID;
+                                                          ";
+
+            SqlConnection Connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            SqlCommand Coomand = new SqlCommand(query, Connection);
+            int rowEffected = 0;
+            try
+            {
+
+                Connection.Open();
+                rowEffected = Coomand.ExecuteNonQuery();
+
+          
+            }
+            catch (Exception ex) { }
+            finally { Connection.Close(); }
+
+            return rowEffected > 0;
+            
+
+        }
+
+        public static bool GetDriverInfoByPersonID(ref int DriverID,int personID, ref int CreatedByUserID,
+            ref DateTime CreateDate)
+        {
+            string query = "select * from Drivers where PersonID = @PersonId";
+
+            SqlConnection Connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            SqlCommand Coomand = new SqlCommand(query, Connection);
+            Coomand.Parameters.AddWithValue("@PersonId", personID);
+            bool isFound = false;
+
+            try
+            {
+
+                Connection.Open();
+                SqlDataReader reader = Coomand.ExecuteReader();
+
+                while (reader.HasRows)
+                {
+
+                    DriverID = Convert.ToInt32(reader["DriverID"]);
+                    CreatedByUserID = Convert.ToInt32(reader["CreatedByUserID"]);
+                    CreateDate = Convert.ToDateTime(reader["CreateDate"]);
+                    isFound = true;
+                }
+                reader.Close();
+
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+
+                Connection.Close();
+            }
+            return isFound;
+
+
+
+        }
+
+    }
+
+    }
+
+    
+
+    
+
